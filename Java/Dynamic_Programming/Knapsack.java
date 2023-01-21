f6214b257166e507ec545656b0d0dd3d07c159fa
Vamsi Krishna <vamsitupakula474@gmail.com>
2023-01-21 16:38:39 +0530

    knapsack problem using dp (without memoization)

---
+++ Java/Dynamic_Programming/Knapsack.java
@@ -0,0 +1,41 @@
+package Java.Dynamic_Programming;
+
+import java.util.*;
+
+/**
+ * ! Problem Statement:
+ * Given a set of items where each item contains a weight and value, determine the number of each to include in
+ * a collection so that the total weight is less than or equal to a given limit and the total value is as large as
+ * possible and return the max profit
+ */
+
+public class Knapsack {
+    public static void main(String[] args) {
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item(40, 100));
+        items.add(new Item(20, 39));
+        items.add(new Item(20, 39));
+        items.add(new Item(15, 29));
+
+        System.out.println(knapsack(60, items, 0));
+    }
+
+    private static int knapsack(int weight_left, ArrayList<Item> items, int itemNo) {
+        if(itemNo >= items.size() || weight_left == 0) return 0;
+        if(weight_left < items.get(itemNo).weight)
+            return knapsack(weight_left, items, itemNo+1);
+
+        int case_1 = items.get(itemNo).value + knapsack(weight_left - items.get(itemNo).weight, items, itemNo+1);
+        int case_2 = knapsack(weight_left, items, itemNo+1);
+        return Math.max(case_1, case_2);
+    }
+}
+
+class Item {
+    int weight;
+    int value;
+    public Item(int weight, int value) {
+        this.value = value;
+        this.weight = weight;
+    }
+}
