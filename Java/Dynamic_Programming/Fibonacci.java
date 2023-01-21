ec0c9b42d6a8a55a1c374d867a12d2c1718dd153
Vamsi Krishna <vamsitupakula474@gmail.com>
2023-01-21 14:49:08 +0530

    dynamic programming folder added and fibonacci using dp moved into it...

--- Java/Dynamic_Programming/Fibonacci.java
+++ Java/Dynamic_Programming/Fibonacci.java
@@ -1,17 +1,16 @@
-package Java;
+package Java.Dynamic_Programming;
 
 import java.util.*;
 
-public class FibonacciUsingDP {
+public class Fibonacci {
     public static void main(String[] args) {
-        int n = 100;
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<n;i++) {
-            System.out.println(i + " " + fibo(i, map));
+        for(int i=0;i<=100;i++) {
+            System.out.println(i + " : " + fibo(i, map));
         }
     }
 
-    private static int fibo(int n, Map<Integer, Integer> map) {
+    public static int fibo(int n, Map<Integer, Integer> map) {
         if(n == 0 || n == 1) return n;
         if(map.containsKey(n)) return map.get(n);
         map.put(n, fibo(n-1, map) + fibo(n-2, map));
