d979d4fff320981a2447fedcc9e1313681e337ba
Vamsi Krishna <vamsitupakula474@gmail.com>
2023-02-02 08:35:34 +0530

    print inorder of a binary tree

--- Java/BinaryTrees/BinaryTree.java
+++ Java/BinaryTrees/BinaryTree.java
@@ -24,11 +24,24 @@ public class BinaryTree {
         printPreorder(root.right);
     }
 
+    public static void printInorder(TreeNode root) {
+        if (root == null)
+            return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+        printInorder(root.right);
+    }
+
     public static void main(String[] args) {
         // preorder sequence
         int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         TreeNode root = buildTreePreOrder(nodes);
 
+        System.out.println("preorder sequence : ");
         printPreorder(root);
+        System.out.println();
+        System.out.println("inorder sequence : ");
+        printInorder(root);
     }
 }
